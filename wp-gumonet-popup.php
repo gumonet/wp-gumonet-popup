110dd81edd7f2bc75b2f71de5dd996a35a014ccd
Angel Gutierrez Monola <gutierrez.monola@gmail.com>
2021-08-10 18:54:05 -0500

    Custom popups

---
+++ wp-gumonet-popup.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Gumonet Popup
+Plugin URI:   http://gumonet.com
+Description:  Agrega un popup a tu wordpress
+Version:      0.0.1
+Author:       Gumonet.com
+Author URI:   https://www.gumonet.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wp-gm-popup
+Domain Path:  /languages
+*/
